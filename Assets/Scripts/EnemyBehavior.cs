7d253d1379f70ddb0745772cbffaabf69b5f96e4
DamiCassinotti <damian.cassinotti@despegar.com>
2021-11-23 22:53:23 -0300

    Add patrolling to NPC

--- Assets/Scripts/EnemyBehavior.cs
+++ Assets/Scripts/EnemyBehavior.cs
@@ -8,6 +8,7 @@ public class EnemyBehavior : MonoBehaviour
 
     [SerializeField] Transform target;
     [SerializeField] float chaseRange = 10f;
+    [SerializeField] float walkPointRange = 10f;
 
     NavMeshAgent navMeshAgent;
     float distanceToTarget = Mathf.Infinity;
@@ -23,47 +24,21 @@ public class EnemyBehavior : MonoBehaviour
         if (distanceToTarget <= chaseRange)
         {
             navMeshAgent.SetDestination(target.position);
+        } else {
+            navMeshAgent.SetDestination(GetNextDestination());
         }
+    }
+
+    Vector3 GetNextDestination()
+    {
+        float randomZ = Random.Range(-walkPointRange, walkPointRange);
+        float randomX = Random.Range(-walkPointRange, walkPointRange);
+        return new Vector3(transform.position.x + randomX, transform.position.y, transform.position.z + randomZ);
     }
 
     void OnDrawGizmosSelected() {
         Gizmos.color = Color.white;
         Gizmos.DrawWireSphere(transform.position, chaseRange);
     }
-
-    /*[SerializeField]  LayerMask whatIsGround, whatIsPlayer;
-
-    //States
-    [SerializeField] float sightRange, attackRange;
-    bool playerInSightRange, playerInAttackRange;
-    void Start()
-    {
-
-    }
-
-    void Update()
-    {
-        //Check for sight and attack range
-        playerInSightRange = Physics.CheckSphere(transform.position, sightRange, whatIsPlayer);
-        //playerInAttackRange = Physics.CheckSphere(transform.position, attackRange, whatIsPlayer);
-
-        if (!playerInSightRange && !playerInAttackRange) Patroling();
-        if (playerInSightRange && !playerInAttackRange) ChasePlayer();
-        //if (playerInAttackRange && playerInSightRange) AttackPlayer();
-    }
-
-    void Patroling()
-    {
-        if (!walkPointSet) SearchWalkPoint();
-
-        if (walkPointSet)
-            agent.SetDestination(walkPoint);
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        //Walkpoint reached
-        if (distanceToWalkPoint.magnitude < 1f)
-            walkPointSet = false;
-    }*/
 
 }
--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,6 +9,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] float range = 100f;
     [SerializeField] float damage = 50f;
     [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect;
 
     void Update()
     {
@@ -29,12 +30,23 @@ public class Weapon : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
-            Health target = hit.transform.GetComponent<Health>();
-            if (target != null)
-            {
-                target.DecreaseHealth(damage);
-            }
-            Debug.Log("I hit this thing: " + hit.transform.name);
+            CreateHitImpact(hit);
+            ProcessHitEnemy(hit);
         }
+    }
+
+    void ProcessHitEnemy(RaycastHit hit)
+    {
+        Health target = hit.transform.GetComponent<Health>();
+        if (target != null)
+        {
+            target.DecreaseHealth(damage);
+        }
+    }
+
+    void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, .1f);
     }
 }
