be60724c72d5328ac706a2254d1d637b256dde5e
DamiCassinotti <damian.cassinotti@despegar.com>
2021-12-13 16:02:19 -0300

    Bug fix in finished round menu

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,24 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using TMPro;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] TMP_Text winnerText;
     [SerializeField] Canvas finishedRoundCanvas;
     Counter counter;
+    FirstPersonController fpController;
     string playerTag = "Player";
     string npcTag = "NPC";
 
     void Start()
     {
         counter = FindObjectOfType<Counter>();
+        fpController = GameObject.FindWithTag(playerTag).GetComponent<FirstPersonController>();
         finishedRoundCanvas.enabled = false;
     }
 
     public void LoadNewLevel()
     {
-        Debug.Log("Reloading scene");
         SceneManager.LoadScene(0);
         Time.timeScale = 1;
     }
@@ -53,6 +55,7 @@ public class GameManager : MonoBehaviour
     void FinishRound(string winner)
     {
         winnerText.text = winner + " wins!";
+        fpController.enabled = false;
         finishedRoundCanvas.enabled = true;
         Time.timeScale = 0;
         Cursor.lockState = CursorLockMode.None;
